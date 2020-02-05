145a86d65444dbaccc7f16a76a4664e7db2283df
natewolford <natenwolford@gmail.com>
2020-02-05 15:02:50 -0800

    Initial commit

---
+++ src/MerkleManager_Test.java
@@ -0,0 +1,6 @@
+public class MerkleManager_Test {
+    public static void main(String[] args) {
+        MerkleManager oMerkleManager = new MerkleManager();
+        MerkleManager.manage();
+    }
+}
---
+++ src/MonitorThread.java
@@ -0,0 +1,28 @@
+public class MonitorThread implements Runnable {
+
+    MerkleManager oMerkleManager = new MerkleManager();
+    Util oUtil = new Util();
+    String threadName;
+
+    @Override
+    public void run() {
+        while(true) {
+            if(MerkleManager.smerkleRoot != null) {
+                if(MerkleManager.smerkleRoot.equals(MerkleManager.expectedRoot)) {
+                    System.out.println("You win! Merkle Root: " + MerkleManager.smerkleRoot);
+                    System.exit(0);
+                }else if (MerkleManager.smerkleRoot != MerkleManager.expectedRoot) {
+                    System.out.println("You lost!");
+                    System.exit(0);
+                }
+            }
+            else if (MerkleManager.strikes == 3) {
+                System.out.println("3 Strikes you lost!");
+                System.exit(0);
+            }
+            oUtil.sleep(1);
+        }
+
+    }
+}
+
